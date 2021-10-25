edb7e600c94ab1a03137a43c3ef774ed731bf0a3
Srikanth Pothedar <spothedar@guardanthealth.com>
2021-10-25 15:44:32 -0700

    SO2C-1056 [SP] remove sudo

--- scripts/SetupGithubAuth.sh
+++ scripts/SetupGithubAuth.sh
@@ -6,5 +6,5 @@ echo host=github.com
 echo username=foo
 echo password=${GITHUB_PERSONAL_ACCESS_TOKEN}
 EOF
-sudo install creds /usr/local/bin/creds
-#git config --global credential.helper "creds"
+install creds /usr/local/bin/creds
+git config --global credential.helper "creds"
